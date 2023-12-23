928a038fb822e5f5fdf8cf51d33e1d68c793c032
charlesojihoji <charlesojihoji@gmail.com>
2023-12-23 12:50:50 -0500

    project initialized

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'EmpApp';
+  counter = 0;
+
+  changeTitle(){
+    this.title = "Changed Title";
+  }
+
+  increment(){
+    this.counter++;
+  }
+
+  decrement(){
+    this.counter--;
+  }
+
+  reset(){
+    this.counter = 0;
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+<<<<<<< HEAD
+  content: [ "./src/**/*.{html,ts}", ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+=======
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+>>>>>>> a7ab8d791b4fe6d8a9434f53d25e75f02f9b620c
+}
+
