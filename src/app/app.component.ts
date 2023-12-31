51019dfb4c1f7ab04986af48461a4179fae3aa10
charlesojihoji <charlesojihoji@gmail.com>
2023-12-31 12:56:00 -0500

    Header component created and updated CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './common/header/header.component';
 
-@Component({
+@Component( {
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [ CommonModule, RouterOutlet, HeaderComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
-})
+} )
 export class AppComponent {
   title = 'EmpApp';
   counter = 0;
 
-  changeTitle(){
+  changeTitle () {
     this.title = "Changed Title";
   }
 
-  increment(){
+  increment () {
     this.counter++;
   }
 
-  decrement(){
+  decrement () {
     this.counter--;
   }
 
-  reset(){
+  reset () {
     this.counter = 0;
   }
 }
